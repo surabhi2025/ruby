83e7f41677d37a84de42fca99714008b63c88927
Surabhi Mathur <surabhimathur27@gmail.com>
2025-09-08 18:39:09 -0400

    Birthday Project

---
+++ main.py
@@ -0,0 +1,11 @@
+Surabhi = "August 4 2014"
+Sachin = "October 26 1979"
+Ruby = "March 10 2024"
+Ashita = "November 28 1984"
+Vijay = " April 2 1949"
+
+print(Surabhi)
+print(Sachin)
+print(Ruby)
+print(Ashita)
+print(Vijay)
