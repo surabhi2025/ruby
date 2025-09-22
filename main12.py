9698b3b00df498da03dd214961e9014db45994c3
Surabhi Mathur <surabhimathur27@gmail.com>
2025-09-22 19:25:09 -0400

    square root

---
+++ main12.py
@@ -0,0 +1,15 @@
+number = float(input("Please enter any number you want:"))
+
+
+
+root = number ** (1/2) 
+print(root)
+print(number)
+
+x = root ** (2)
+print(x)
+
+import math
+r = math.sqrt(number)
+print(r)
+
