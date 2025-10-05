42dd15130c7242b672288b99c633172cc1f1106f
Surabhi Mathur <surabhimathur27@gmail.com>
2025-10-05 16:04:48 -0400

    grades

---
+++ main19.py
@@ -0,0 +1,11 @@
+grade = int(input("Enter your grade:"))
+if (grade > 90):
+    print("You get a A")
+elif (grade > 80):
+    print("You get a B")
+elif (grade > 70):
+    print("You get a C")
+elif (grade > 60):
+    print("You get a D")
+else:
+    print("You get a F-")
